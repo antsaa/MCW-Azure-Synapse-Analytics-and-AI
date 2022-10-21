c113b14b6487d0dea0bdb52ff0ef3a40e338b795
Harald Solstad Fianbakken <harald.fianbakken@gmail.com>
2022-10-21 08:30:58 +0200

    Updated readme

--- Hands-on lab/environment-setup/automation/Setup-all.ps1
+++ Hands-on lab/environment-setup/automation/Setup-all.ps1
@@ -2,10 +2,11 @@ $rgName = 'hsf-synapse-demo2'
 $rg = new-azresourcegroup $rgName -location 'west europe'
 
 $suffix = 'hsf';
-#$sqlPassword = Read-Host -Prompt "Enter the SQL Administrator password you used in the deployment" -AsSecureString
-$sqlPassword = 'P@ssw0rd1234!!'|ConvertTo-SecureString -AsPlainText -force
+$sqlPassword = Read-Host -Prompt "Enter the SQL Administrator password you used in the deployment" -AsSecureString
+# If you're lazy and don't like t
+#$sqlPassword = 'P@ssw0rd1234-/!!'|ConvertTo-SecureString -AsPlainText -force
 $sqlPassword1 = [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($sqlPassword))
-$uri = 'https://raw.githubusercontent.com/microsoft/MCW-Azure-Synapse-Analytics-and-AI/main/Hands-on%20lab/environment-setup/automation/00-asa-workspace-core.json'
+$uri = 'https://raw.githubusercontent.com/haraldfianbakken/MCW-Azure-Synapse-Analytics-and-AI/main/Hands-on%20lab/environment-setup/automation/00-asa-workspace-core.json'
 
 $deployment = New-AzResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -TemplateUri $uri -uniqueSuffix $suffix -sqlAdministratorLoginPassword $sqlPassword
 ./01-environment-setup.ps1 -resourceGroupName $rg.ResourceGroupName -SQLPassword $sqlPassword1 -suffix $suffix
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Azure Synapse Analytics and AI
 
-## This workshop is archived and no longer being maintained. Content is read-only.
+## The hands-on component are untested in a while. Use at caution.
 
 Wide World Importers (WWI) has hundreds of brick-and-mortar stores and an online store where they sell a variety of products. They would like to gain business insights using historical, real-time, and predictive analytics using structured and unstructured data sources. In doing so, they want to enable their IT team of data engineers and data scientists to bring in and run complex queries over petabytes of structured data with billions of rows and unstructured enterprise operational data. At the same time, they want to enable business analysts and their IT team to share a single source of truth and have a single workspace to collaborate and work with enterprise data and enriched customer data. They want to accomplish this by minimizing the number of disparate services they use across ingest, transformation, querying, and storage so that their team of data engineers, data scientists, and database administrators can master one tool, and can build shared best practices for development, management, and monitoring.
 
