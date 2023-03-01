d3990f00eaa1bf9b02172a86218d02f4400e113e
DibakarMSFT <dibakardharchoudhury@yahoo.co.in>
2023-03-01 16:43:40 +0100

    Update setup script

--- Hands-on lab/environment-setup/automation/Setup-all.ps1
+++ Hands-on lab/environment-setup/automation/Setup-all.ps1
@@ -64,8 +64,21 @@ $rg = new-azresourcegroup $rgName -location 'west europe'
 
 
 #Synapse SQL Pool Password
+""
+Write-Host "================================================================" -ForegroundColor Cyan
+Write-Host "Note: Synapse SQL Pool Password requirements are as follows: "  -ForegroundColor Cyan
+Write-Host "================================================================" -ForegroundColor Cyan
+Write-Host "(1) Your password must be between 8 and 128 characters long." -ForegroundColor Brown
+Write-Host "(2) Your password must contain characters from three of the following categories: 
+                `nEnglish uppercase letters, 
+                `nEnglish lowercase letters, 
+                `nnumbers (0-9), 
+                `nand non-alphanumeric characters (!, $, #, %, etc.)." -ForegroundColor Brown
+Write-Host "(3) Your password cannot contain all or part of the login name. Part of a login name is defined as three or more consecutive alphanumeric characters." -ForegroundColor Brown
+""
 $sqlPassword = Read-Host -Prompt " `nEnter the Synapse SQL Pool Administrator password, please remember the same for the Labs " -AsSecureString
 $sqlPassword1 = [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($sqlPassword))
+
 
 ""
 Write-Host "====================================================" -ForegroundColor Cyan
@@ -74,13 +87,15 @@ Write-Host "====================================================" -ForegroundCol
 
 
 $uri = 'https://raw.githubusercontent.com/dibakardharchoudhury/MCW-Azure-Synapse-Analytics-and-AI/main/Hands-on%20lab/environment-setup/automation/00-asa-workspace-core.json'
-$deployment = New-AzResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -TemplateUri $uri -uniqueSuffix $suffix -sqlAdministratorLoginPassword $sqlPassword
+#$deployment = New-AzResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -TemplateUri $uri -uniqueSuffix $suffix -sqlAdministratorLoginPassword $sqlPassword
+New-AzResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -TemplateUri $uri -uniqueSuffix $suffix -sqlAdministratorLoginPassword $sqlPassword
 
+""
+""
 
 Write-Host "================================================================" -ForegroundColor Cyan
 Write-Host "SETTING UP ENVIRONMENT & COPYING DATA....PLEASE BE PATIENT... "  -ForegroundColor Cyan
 Write-Host "================================================================" -ForegroundColor Cyan
 
 ./01-environment-setup.ps1 -resourceGroupName $rg.ResourceGroupName -SQLPassword $sqlPassword1 -suffix $suffix
-
 
